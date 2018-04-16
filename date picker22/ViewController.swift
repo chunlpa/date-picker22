a636225fc15ca037ac3acb6b9ad8d6e810359662
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-04-16 15:19:38 +0900

    add action

--- date picker22/ViewController.swift
+++ date picker22/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     var myTimer = Timer()
     var count = 0
     let interval = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,10 +25,14 @@ class ViewController: UIViewController {
     
     @IBAction func picker1(_ sender: Any) {
         print("change Date Picker")
-        
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss a EE"
+        formatter.dateFormat = "HH:mm:ss EEE"
         label.text = formatter.string(from: picker1.date)
+        
+        
+        
+        
+        
         
     }
     @objc func updateTime() {
@@ -35,6 +40,32 @@ class ViewController: UIViewController {
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm:ss EEE"
         NTP.text = formatter.string(from: date as Date)
+        
+        if label.text == NTP.text {
+            view.backgroundColor = UIColor.green
+            let alertController = UIAlertController(title: "제목", message: "내용부분입니다.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let DestructiveAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.destructive) { (result : UIAlertAction) -> Void in
+                
+                print("취소")
+                
+            }
+            
+            let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+                
+                print("확인")
+                
+            }
+            
+            alertController.addAction(DestructiveAction)
+            
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+            
+    
+        }
     }
     
 }
